25faa013db1b219c179c0fd8e1e73eef61bd59f9
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-03 17:27:45 -0600

    Se creo la constante maximoRecetas en TestRecetas.java linea 13

--- src/controlador/TestRecetas.java
+++ src/controlador/TestRecetas.java
@@ -10,6 +10,11 @@ public class TestRecetas {
      * @param args the command line aguments
      */
     public static void main(String[] args){
+        final int maximoRecetas=10;
+        /*cantidad máxima de recetas que se pueden almacenar
+        final se utiliza para indicar que la variable maximoRecetas es una 
+        constante por lo que no cambiará en la ejecución del programa
+        */
         
     }
 }
