151ba1481a9537e0a691d2c2ecd85e83f1ef33f4
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-03 16:45:22 -0600

    Se crearon los paquetes controlador,modeloDatos y recursos. Se creo la clase TestRecetas.java en el paquete controlador (esta es la clase principal del programa)

---
+++ src/controlador/TestRecetas.java
@@ -0,0 +1,15 @@
+
+package controlador;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public class TestRecetas {
+    /**
+     * @param args the command line aguments
+     */
+    public static void main(String[] args){
+        
+    }
+}
