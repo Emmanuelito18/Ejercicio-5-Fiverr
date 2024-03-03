40fe2bafeaac7d0b347eed32efc483740bbb2cdb
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-03 15:42:22 -0600

    Commit inicial

---
+++ src/ejercicio/pkg5/fiverr/Ejercicio5Fiverr.java
@@ -0,0 +1,13 @@
+
+package ejercicio.pkg5.fiverr;
+
+public class Ejercicio5Fiverr {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
