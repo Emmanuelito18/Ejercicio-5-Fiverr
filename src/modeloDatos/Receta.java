1c4ef89d691cd700a20c8329a43182d924a8c438
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-03 21:47:04 -0600

    Se añadieron los atributos y moetodos de la clase Paciente.java falta poner el atributo heredado linea 6. falta poner el atributo de fecha local linea 10

--- src/modeloDatos/Receta.java
+++ src/modeloDatos/Receta.java
@@ -1,5 +1,79 @@
 package modeloDatos;
 
-public class Receta {
+public class Receta extends Paciente{
+    //Atributos
+    String identificador;
+    //Espacio para atributo heredado
+    String medicamento;
+    char formato;//p=pastilla i=inyectable s=supositorio
+    String  psologia;
+    //espacio para fecha local
+    int numColegiado;
+    int totalRecetas;
+    
+    //Métodos
+
+    //Métodos cosntructores
+    public Receta() {
+    }
+
+    public Receta(String identificador, String medicamento, char formato, String psologia, int numColegiado, int totalRecetas, String nif, String nombre, String apellidos, int telefono) {
+        super(nif, nombre, apellidos, telefono);
+        this.identificador = identificador;
+        this.medicamento = medicamento;
+        this.formato = formato;
+        this.psologia = psologia;
+        this.numColegiado = numColegiado;
+        this.totalRecetas = totalRecetas;
+    }
+    
+    //métodos getter y setters
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    public String getMedicamento() {
+        return medicamento;
+    }
+
+    public void setMedicamento(String medicamento) {
+        this.medicamento = medicamento;
+    }
+
+    public char getFormato() {
+        return formato;
+    }
+
+    public void setFormato(char formato) {
+        this.formato = formato;
+    }
+
+    public String getPsologia() {
+        return psologia;
+    }
+
+    public void setPsologia(String psologia) {
+        this.psologia = psologia;
+    }
+
+    public int getNumColegiado() {
+        return numColegiado;
+    }
+
+    public void setNumColegiado(int numColegiado) {
+        this.numColegiado = numColegiado;
+    }
+
+    public int getTotalRecetas() {
+        return totalRecetas;
+    }
+
+    public void setTotalRecetas(int totalRecetas) {
+        this.totalRecetas = totalRecetas;
+    }
     
 }
