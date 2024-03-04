a0199220c0b01ef41ac781db9d149fb1ca2474aa
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-03 21:45:02 -0600

    Se añadieron los atributos y moetodos de la clase TestRecetas.java

--- src/controlador/TestRecetas.java
+++ src/controlador/TestRecetas.java
@@ -1,20 +1,38 @@
-
 package controlador;
 
 /**
  *
- * @author Emmanuel
+ * @author Emmanuelito18
  */
 public class TestRecetas {
     /**
      * @param args the command line aguments
      */
     public static void main(String[] args){
-        final int maximoRecetas=10;
+        //Recuerda que si puedes imaginarlo puedes programarlo
+        
+        final byte numRecetas=10;
         /*cantidad máxima de recetas que se pueden almacenar
         final se utiliza para indicar que la variable maximoRecetas es una 
         constante por lo que no cambiará en la ejecución del programa
         */
+        
+        Receta[] recetas=new Receta[numRecetas];
+    }
+    public byte menu(){
+        
+    }
+    public void agregarReceta(){
+        
+    }
+    public void modificarReceta(){
+        
+    }
+    public void eliminarReceta(){
+        
+    }
+    
+    public void inicializar(){
         
     }
 }
