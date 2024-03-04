d1a14b23b8faf4f3ad6e6599da76df767da7e9a2
Emmanuelito18 <emmanuelito18yt@gmail.com>
2024-03-03 21:45:56 -0600

    Se añadieron los atributos y moetodos de la clase Paciente.java

--- src/modeloDatos/Paciente.java
+++ src/modeloDatos/Paciente.java
@@ -1,5 +1,63 @@
 package modeloDatos;
 
 public class Paciente {
+    //Atributos
+    String nif;
+    String nombre;
+    String apellidos;
+    int telefono;
+    //localdate fechaNacimiento;
     
+    //métodos
+    
+    //método constructor vacio
+    public Paciente() {
+    }
+    
+    //método constructor inicializado
+    public Paciente(String nif, String nombre, String apellidos, int telefono) {
+        this.nif = nif;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    
+    //falta método getter y setter para fechaNacimiento
+    
+    public String toString(){
+        String cadena=null;
+        return cadena;
+    }
 }
